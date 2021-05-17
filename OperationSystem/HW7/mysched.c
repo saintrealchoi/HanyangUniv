f93c0c508d4bad8bb4c98ba79978c020e0d4ff8e
sungjinchoi <csj7480@gmail.com>
2021-05-17 23:33:20 +0900

    Finish simple scheduler

--- OperationSystem/HW7/mysched.c
+++ OperationSystem/HW7/mysched.c
@@ -1,64 +1,63 @@
 #include "sched.h"
-
 void init_mysched_rq(struct mysched_rq *mysched_rq)
 {
 	printk(KERN_INFO "***[MYSCHED] Myschedclass is online \n");
-	mysched_rq->nr_running= 0;
 	INIT_LIST_HEAD(&mysched_rq->queue);
 }
 
 static void update_curr_mysched(struct rq *rq)
 {
 }
-
-//TODO
 static void enqueue_task_mysched(struct rq *rq, struct task_struct *p, int flags) 
 {
-	list_add_tail(&p->mysched.run_list,&rq->mysched.queue);
-	rq->mysched.nr_running++;
-	printk(KERN_INFO"***[MYSCHED] enqueue: success cpu=%d,nr_running=%d,p->state=%ld,p->pid=%d\n",p->on_cpu,rq->mysched.nr_running,p->state,p->pid);
+	struct mysched_rq *mysched_rq = &rq->mysched;
+	struct sched_mysched_entity *mysched_entity = &p->mysched;
+	list_add_tail(&mysched_entity->run_list, &mysched_rq->queue);
+	mysched_rq->nr_running++;
+
+	printk(KERN_INFO"***[MYSCHED] enqueue: success cpu=%d,nr_running=%d,p->state=%d,p->pid=%d\n",cpu_of(rq),mysched_rq->nr_running,p->state,p->pid);
 
 }
 
-//TODO
 static void dequeue_task_mysched(struct rq *rq, struct task_struct *p, int flags)
 {
-
 	printk(KERN_INFO"***[MYSCHED] dequeue: start\n");
-	if((int)rq->mysched.nr_running >0){
-		printk(KERN_INFO "***[MYSCHED] dequeue: the dequeued task is curr, set TIF_NEED_RESCHED flag cpu=%d,p->state=%ld,p->pid=%d,curr->pid=%d\n",p->on_cpu,p->state,p->pid,p->pid);
-		list_del_init(&p->mysched.run_list);
-		rq->mysched.nr_running--;
-	}
+	struct mysched_rq* mysched_rq = &rq->mysched;
+	struct sched_mysched_entity* mysched_entity = &p->mysched;
 
+	if(mysched_rq->nr_running > 0){
+		list_del(&mysched_entity->run_list);
+		mysched_rq->nr_running--;
+		printk(KERN_INFO "***[MYSCHED] dequeue: the dequeued task is curr, set TIF_NEED_RESCHED flag cpu=%d,p->state=%d,p->pid=%d,curr->pid=%d\n",cpu_of(rq), p->state, p->pid, p->pid);
+	}
 	else{}
+
 	printk(KERN_INFO"***[MYSCHED] dequeue: end\n");
 
 }
 static void check_preempt_curr_mysched(struct rq *rq, struct task_struct *p, int flags) 
 { 
 }
-//TODO
 struct task_struct *pick_next_task_mysched(struct rq *rq, struct task_struct *prev)
 {
-	struct task_struct *p;
-	struct sched_mysched_entity *mysched_entity = NULL;
+	struct task_struct* next_p = NULL;
+	struct sched_mysched_entity* next_entity = NULL;
+	struct mysched_rq* mysched_rq = &rq->mysched;
+	struct list_head* queue = &mysched_rq->queue;
 
-	if(list_empty(rq->mysched.queue.next)){
+	if(mysched_rq->nr_running == 0){
 		return NULL;
 	}
-	else{
-		put_prev_task(rq,prev);
-		mysched_entity = list_entry(rq->mysched.queue.next, struct sched_mysched_entity, run_list);
-		p = container_of(mysched_entity, struct task_struct, mysched);
-		printk(KERN_INFO"***[MYSCHED] pick_next_task: cpu=%d,prev->pid=%d,next_p->pid=%d,nr_running=%d\n",p->on_cpu,prev->pid,p->pid,rq->mysched.nr_running);
-	}
-	printk(KERN_INFO "***[MYSCHED] pick_next_task: end\n");
+
+	next_entity = container_of(queue->next, struct sched_mysched_entity, run_list);
+	next_p = container_of(next_entity,struct task_struct, mysched);
+	printk(KERN_INFO "***[MYSCHED] pick_next_task: cpu=%d, prev->pid=%d, next_p->pid=%d, nr_running=%d\n",cpu_of(rq),prev->pid,next_p->pid,mysched_rq->nr_running);
+	return next_p;
+
 }
-//TODO
 static void put_prev_task_mysched(struct rq *rq, struct task_struct *p) 
 {
-	printk(KERN_INFO "***[MYSCHED] put_prev_task: do nothing,p->pid=%d\n",p->pid);
+	printk(KERN_INFO "***[MYSCHED] put_prev_tas: do nothing, p->pid=%d\n",p->pid);
 }
 static int select_task_rq_mysched(struct task_struct *p, int cpu, int sd_flag, int flags)
 {
